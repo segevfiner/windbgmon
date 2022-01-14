878a68be4829fdd79aa4fde2527c16f0ae3c6917
Segev Finer <segev208@gmail.com>
2022-01-14 16:12:41 +0200

    Add sphinx.ext.githubpages

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ release = windbgmon.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
